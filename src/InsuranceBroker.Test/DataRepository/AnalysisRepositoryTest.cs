e61b223c85e99b4558c48408a2d28c01357e59db
Bilal Ahmed <bilal.awan@gmail.com>
2016-03-09 00:23:46 +0100

    Added projects for the repositories, domain and test

---
+++ src/InsuranceBroker.Test/DataRepository/AnalysisRepositoryTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace InsuranceBroker.Test.DataRepository
+{
+    [TestClass]
+    public class AnalysisRepositoryTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
