8fc9c51dd9f3652202f879fd09919351b2d90166
unknown <bah@PITNBBAH-WIN10.pit.dk>
2016-03-09 15:33:35 +0100

    Test classes created in domain project

---
+++ src/InsuranceBroker.Domain/Models/Analysis.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InsuranceBroker.Domain.Models
+{
+    public enum AnalysisState
+    {
+        Created = 0, 
+        InProgress, 
+        Submitted, 
+        Closed
+    }
+
+    public class Analysis
+    {
+        public System.Guid Id { get; set; }
+        public System.Guid AnalysisDefinitionId { get; set; }
+        public System.Guid CustomerId { get; set; }
+        public string CustomerName { get; set; }
+        public Nullable<System.Guid> UserId { get; set; }
+        public string UserName { get; set; }
+        public AnalysisState AnalysisState { get; set; }
+    }
+}
---
+++ src/InsuranceBroker.Domain/Repositories/IAnalysisRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using InsuranceBroker.Domain.Models;
+
+namespace InsuranceBroker.Domain.Repositories
+{
+    public interface IAnalysisRepository
+    {
+        List<Analysis> GetAllByUserId(Guid userId);
+
+        Analysis Create(Analysis analysis);
+
+        Analysis Update(Analysis analysis);
+
+        bool Delete(Guid analysisId);
+    }
+}
